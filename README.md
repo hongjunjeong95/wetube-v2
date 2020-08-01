ba22c1b17b41e05e1162ef39d48592dd0f8fcac1
zpskek <wjdghdwns@gmail.com>
2020-08-01 09:56:24 +0900

    Set development environment

---
+++ README.md
@@ -0,0 +1,5 @@
+# WeTube
+
+Cloning Youtube with VanillaJS and NodeJS
+
+## Pages:
---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// helmet은 application을 보다 더 안전하게 만들어준다.
+app.use(helmet());
+
+// app을 이용해서 middleware를 추가해준다.
+// cookieParser는 사용자 인증에 필요한 cookie를 전달 받는다.
+app.use(cookieParser());
+
+// bodyParser는 사용자가 웹사이트로 전달하는 정보들(request 정보에서)
+// form이나 JSON 형태로 된 body를 검사한다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// logging middleware
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+export default app;
